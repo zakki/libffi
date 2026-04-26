95758dcd5efe2cef85817a451793104bdbec9157
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2026-04-26 18:15:55 +0900

    Fix FFI_ALIGN_DOWN error on MSVC ARM64

--- include/ffi_common.h
+++ include/ffi_common.h
@@ -117,7 +117,7 @@ void ffi_type_test(ffi_type *a, const char *file, int line);
 /* v cast to size_t and aligned up to a multiple of a */
 #define FFI_ALIGN(v, a)  (((((size_t) (v))-1) | ((a)-1))+1)
 /* v cast to size_t and aligned down to a multiple of a */
-#define FFI_ALIGN_DOWN(v, a) (((size_t) (v)) & -a)
+#define FFI_ALIGN_DOWN(v, a) (((size_t) (v)) & ~((a) - 1))
 
 /* Perform machine dependent cif processing */
 ffi_status ffi_prep_cif_machdep(ffi_cif *cif);
